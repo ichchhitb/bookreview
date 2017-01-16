42008452e36de7fd4dc9bdb4bcedc3ac1f2c96d6
ankitamaha <ankitamaha@cybage.com>
2017-01-16 12:24:00 +0530

    commitng property file

--- src/database/ConnectionFactory.java
+++ src/database/ConnectionFactory.java
@@ -1,30 +1,41 @@
 package database;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
 public class ConnectionFactory {
 	static Logger log = Logger.getLogger(BookDAO.class);
-	
-	private ConnectionFactory()
-	{
-		
+
+	private ConnectionFactory() {
+
 	}
+
 	/**
 	 * getConnection() method to establish the database connection
+	 * 
 	 * @return connection
 	 */
 	public static Connection getConnection() {
 		log.info("Inside getConnection() ");
+		Properties properties = new Properties();
+		InputStream inputStream = ConnectionFactory.class.getClassLoader().getResourceAsStream("config.properties");
 		try {
-			
-			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection("jdbc:mysql://localhost/bookreview", "root", "root");
-		} catch (ClassNotFoundException | SQLException e) {
-			log.error("Error inside getConnection() "+e);
+			if (inputStream != null)
+				properties.load(inputStream);
+			String driver = properties.getProperty("driver");
+			String url = properties.getProperty("url");
+			String username = properties.getProperty("username");
+			String password = properties.getProperty("password");
+			Class.forName(driver);
+			return DriverManager.getConnection(url, username, password);
+		} catch (ClassNotFoundException | SQLException | IOException e) {
+			log.error("Error inside getConnection() " + e);
 		}
 		log.info("Exit getConnection() ");
 		return null;
